f55906c6d51f445dbd1baf757bedd239eeeb14ee
jungch15 <58167392+jungch15@users.noreply.github.com>
2022-05-12 12:02:16 +0900

    Update parser.py

--- study/parser.py
+++ study/parser.py
@@ -7,14 +7,15 @@ r = requests.get(url)
 
 html = r.text
 soup = BeautifulSoup(html, 'html.parser')
-'''for link in soup.find_all('a'):
+'''
+for link in soup.find_all('a'):
     print(link.get('href'))
 print(r.status_code)
 print(r.headers['Content-Type'])
 print(r.encoding)
 print(r.ok)
 '''
-titles_html = soup.select('.td.td-subject > a > strong')
+titles_html = soup.select('.td.td-subject > a')
 print(titles_html)
 for i in range(len(titles_html)):
     print(i+1, titles_html[i].text)
